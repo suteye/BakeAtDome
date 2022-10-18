1fcaf136e0908baa94dfae11667b79dc8c94ae34
root <root@ip-172-31-92-92.ec2.internal>
2022-10-18 08:10:26 +0000

    add a new file

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(morgan("dev"));
 //routes
 app.use('/api/products', product);
 ///port
-const port = process.env.PORT;
+const port = 5500;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`.yellow.bold);
