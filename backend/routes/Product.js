db85c27c20eee58cd6028e4bf0370a52813d3fda
suteye <sutima.phe@dome.tu.ac.th>
2022-11-05 20:38:28 +0700

    :construction: Work in progres of product stock routes.
    Work in progres of product stock routes.\

--- backend/routes/Product.js
+++ backend/routes/Product.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {metadata, getProducts,addRow,updateValue,deleteRow,getRowsbyId} = require('../controllers/ProductController');
+const {metadata, getProducts,addProduct,updateValue,deleteRow,getRowsbyId} = require('../controllers/ProductController');
 const router = express.Router();
 
 router.get('/metadata',metadata);
 router.get('/getProducts',getProducts);
-router.post('/addRow',addRow);
+router.post('/addProduct',addProduct);
 router.put('/updateValue/:id',updateValue);
 router.post('/deleteRow/:id',deleteRow);
 router.get('/getRowsbyId/:id',getRowsbyId);
