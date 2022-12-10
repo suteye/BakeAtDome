85ff4c9e963d90ad150532634687a1c4b234ac99
suteye <sutima.phe@dome.tu.ac.th>
2022-12-10 22:31:47 +0700

    test ci workflows

--- frontend/test/conf.js
+++ frontend/test/conf.js
@@ -4,6 +4,7 @@ exports.config = {
     },
     framework: 'jasmine',
     // seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect: true,
     specs: ['login_spec.js'],
     jasmineNodeOpts: {
         defaultTimeoutInterval: 20000
