a59be9d79585202f0ed61184a6e4b1ce7e22d4de
suteye <sutima.phe@dome.tu.ac.th>
2022-12-16 01:17:01 +0700

    Update pipeline workflows.

--- backend/test/login.test.js
+++ backend/test/login.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const loginBaseURL = 'http://127.0.0.1/api/auth/';
+const loginBaseURL = 'http://127.0.0.1:5500/api/auth/';
 
 describe('Login & Logout Backend', () => {
 
