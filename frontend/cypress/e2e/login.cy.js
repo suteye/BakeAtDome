9ddb8addb55d71b38f0f1f8094c4629778486a19
suteye <sutima.phe@dome.tu.ac.th>
2022-12-16 00:52:46 +0700

    Update pipeline workflows.

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe('Login', () => {
   it('Should see login page', () => {
-    cy.visit('http://127.0.0.1')
+    cy.visit('http://127.0.0.1:3000')
     cy.url().should('include', '/login')
   })
 
